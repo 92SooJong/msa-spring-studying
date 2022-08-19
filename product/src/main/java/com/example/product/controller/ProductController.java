0e6798f7bbc5f2b008f3faf850666db645b36de2
하수종 <hsz3855@gmail.com>
2022-08-19 18:16:54 +0900

    GetMapping 어노테이션 추가

--- product/src/main/java/com/example/product/controller/ProductController.java
+++ product/src/main/java/com/example/product/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.example.product.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.product.dto.ProductResponseDTO;
 import com.example.product.service.ProductService;
@@ -13,6 +14,7 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @GetMapping("/api/v1/available-products")
     public List<ProductResponseDTO> selectAvailableProducts(){
 
         return productService.selectAvailableProducts();
